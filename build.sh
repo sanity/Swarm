8f51f3bf291bb982f156c3c2d321ac89798ef4c0
Ian Clarke <ian.clarke@gmail.com>
2009-09-01 12:08:45 -0500

    add build script

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+rm -rf bin && \
+mkdir bin && \
+scalac -cp $HOME/local/share/scala/plugin-build/continuations/build/pack/selectivecps-library.jar \
+       -Xpluginsdir $HOME/local/share/scala/plugin-build/continuations/build/pack \
+       -sourcepath src -d bin/ \
+       src/swarm/*.scala
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -cp bin:/Users/ian/local/share/scala/lib/scala-library.jar:/home/ian/local/share/scala/plugin-build/continuations/build/pack/selectivecps-library.jar swarm.Swarm $@
+java -cp bin:$HOME/local/share/scala/lib/scala-library.jar:$HOME/local/share/scala/plugin-build/continuations/build/pack/selectivecps-library.jar swarm.Swarm $@
