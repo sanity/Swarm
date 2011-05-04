d03d2e9c21a249cc3792f9cc038f3b2e1795d061
James Earl Douglas <james@earldouglas.com>
2011-05-04 12:01:58 -0700

    move demos and ContTest from main/ directory to test/ directory

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -6,23 +6,26 @@ class SwarmProject(info: ProjectInfo) extends DefaultProject(info) with AutoComp
   val scalatest = "org.scalatest" % "scalatest" % "1.3"
 
   val cont = compilerPlugin("org.scala-lang.plugins" % "continuations" % "2.8.1")
-  override def compileOptions = 
+
+  override def compileOptions =
     super.compileOptions ++ compileOptions("-P:continuations:enable") ++ compileOptions("-unchecked")
 
-  def getClass(arg: String) = 
+  def getClass(arg: String) =
     if (arg.split(".").size == 0)
       "swarm.demos." + arg
     else arg
 
   lazy val demo =
-   task { args =>
-     if(args.length > 1)
-       actionConstructor(getClass(args(0)), args.toList.tail.toArray)         
-     else
-       task { Some("Usage: Listen|ForceRemoteRef|ExplicitMoveTo1 <integer> <string>") }
-   }
+    task {
+      args =>
+        if (args.length > 1)
+          actionConstructor(getClass(args(0)), args.toList.tail.toArray)
+        else
+          task {
+            Some("Usage: Listen|ForceRemoteRef|ExplicitMoveTo1 <integer> <string>")
+          }
+    }
 
   def actionConstructor(clss: String, args: Array[String]) =
-    runTask(Some(clss), runClasspath, args)
-
+    runTask(Some(clss), testClasspath, args)
 }
