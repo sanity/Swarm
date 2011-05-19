2c66d176a6aae6162e1d79801281e908205b69f9
James Earl Douglas <james@earldouglas.com>
2011-05-18 18:49:29 -0700

    remove some sneaky stowaways

--- swarm-twitter/node1/scala/swarm/twitter/Ports.scala
+++
@@ -1,7 +0,0 @@
-package swarm.twitter
-
-object Ports {
-  def port1: Short = 9997
-
-  def port2: Short = 9998
-}
--- swarm-twitter/node2/scala/swarm/twitter/Ports.scala
+++
@@ -1,7 +0,0 @@
-package swarm.twitter
-
-object Ports {
-  def port1: Short = 9998
-
-  def port2: Short = 9997
-}
