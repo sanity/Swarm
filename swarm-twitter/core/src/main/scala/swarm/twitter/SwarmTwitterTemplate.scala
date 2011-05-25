23a1d41b2e36d391d804cd654e3e1e87ca50d0f2
James Earl Douglas <james@earldouglas.com>
2011-05-24 21:34:25 -0700

    pretty up the Twitter demo

--- swarm-twitter/core/src/main/scala/swarm/twitter/SwarmTwitterTemplate.scala
+++ swarm-twitter/core/src/main/scala/swarm/twitter/SwarmTwitterTemplate.scala
@@ -10,7 +10,9 @@ object SwarmBridge {
   def get(mapKey: String, key: String)(implicit tx: Transporter, local: Location) = {
     Swarm.spawnAndReturn {
       val stringsMap: RefMap[List[String]] = RefMap(classOf[List[String]], mapKey)
-      stringsMap.get(key).getOrElse(Nil).map(x => <div style="margin: 10px; padding: 10px; border-bottom: 1px solid #999;">{x}</div>).toSeq
+      stringsMap.get(key).getOrElse(Nil).map(x => <div style="margin: 10px; padding: 10px; border-bottom: 1px solid #999;">
+        {x}
+      </div>).toSeq
     }
   }
 
@@ -43,13 +45,17 @@ class SwarmTwitterTemplate(localPort: Short, remotePort: Short) extends Scalatra
         <title>SwarmTwitter</title>
       </head>
       <body>
-        <h1>SwarmTwitter</h1>
-        <h2>Sample users:</h2>
-        <div>
-          <a href="/jmcdoe">jmcdoe</a>
-        </div>
-        <div>
-          <a href="/maxpower">maxpower</a>
+        <div style="width: 800px; margin-left: auto; margin-right: auto;">
+          <h1>SwarmTwitter</h1>
+          <p>Welcome to SwarmTwitter, a Twitter simulator built using <a href="https://github.com/sanity/Swarm">Swarm</a>, a framework allowing the creation of web applications which can scale transparently through a novel portable continuation-based approach.</p>
+          <p>To use SwarmTwitter, follow the links below to act as any of the sample users, or add your own users by browsing to <span style="font-face: monospace;">/&lt;username&gt;</span>.</p>
+          <h2>Sample users</h2>
+          <div>
+            <a href="/jmcdoe">jmcdoe</a>
+          </div>
+          <div>
+            <a href="/maxpower">maxpower</a>
+          </div>
         </div>
       </body>
     </html>
@@ -67,18 +73,25 @@ class SwarmTwitterTemplate(localPort: Short, remotePort: Short) extends Scalatra
           <title>SwarmTwitter</title>
         </head>
         <body>
-          <h1>SwarmTwitter</h1>
-          <h2>Statuses:</h2>
-          <div>
-            {SwarmBridge.get(userId, "statuses")}
+          <div style="width: 800px; margin-left: auto; margin-right: auto;">
+            <h1>SwarmTwitter</h1>
+            <h2>
+              {userId}
+            </h2>
+            <h3>Statuses</h3>
+            <div style="margin-bottom: 50px;">
+              {SwarmBridge.get(userId, "statuses")}
+            </div>
+              <hr/>
+            <form action={"/" + userId} method="get">
+              Post a new status:
+                <input type="text" size="15" name="status"/>
+                <input type="submit" value="submit"/>
+            </form>
+            <div><a href={"http://localhost:8080/" + userId}>view node 1</a></div>
+            <div><a href={"http://localhost:8081/" + userId}>view node 2</a></div>
+            <div><a href="/">go home</a></div>
           </div>
-            <hr/>
-          <form action={"/" + userId} method="get">
-            Status:
-              <input type="text" size="15" name="status"/>
-              <input type="submit" value="submit"/>
-          </form>
-          <a href="/">Home</a>
         </body>
       </html>
     }
