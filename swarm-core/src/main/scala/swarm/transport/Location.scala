6bd13e63582d6d144caf7fb063d0d214d496109a
James Earl Douglas <james@earldouglas.com>
2011-06-01 13:45:54 -0700

    add hashCode method to InetLocation

--- swarm-core/src/main/scala/swarm/transport/Location.scala
+++ swarm-core/src/main/scala/swarm/transport/Location.scala
@@ -10,9 +10,14 @@ trait Location extends Serializable
  * A simple Location implementation which uses TCP/IP.
  */
 case class InetLocation(val address: java.net.InetAddress, val port: Short) extends Location {
+
   override def equals(other: Any) = {
     if (!other.isInstanceOf[Any]) false
     val o = other.asInstanceOf[InetLocation]
     address == o.address && port == o.port
+  }
+
+  override def hashCode = {
+    address.hashCode + 37 * port.hashCode
   }
 }
