d88b7ee7e87cb1793d02ce8ffc67692700c720b7
James Earl Douglas <james@earldouglas.com>
2011-05-31 22:34:07 -0700

    cleanup on aisle RefTest

--- swarm-core/src/test/scala/swarm/data/RefTest.scala
+++ swarm-core/src/test/scala/swarm/data/RefTest.scala
@@ -1,29 +1,27 @@
 package swarm.data
 
 import org.scalatest.FunSuite
-import swarm.Swarm.swarm
-import util.continuations._
 import swarm.transport.{Location, Transporter}
 import swarm._
 
 class RefTest extends FunSuite {
 
   test("RefMap") {
-        implicit val tx: Transporter = InMemTest.tx1
-        implicit val local: Location = InMemLocation(1)
+    implicit val tx: Transporter = InMemTest.tx1
+    implicit val local: Location = InMemLocation(1)
 
-        RefMap.locations = List(InMemLocation(1), InMemLocation(2))
+    RefMap.locations = List(InMemLocation(1), InMemLocation(2))
 
-        val stringsMap = RefMap.get(classOf[String], "strings")
+    val stringsMap = RefMap.get(classOf[String], "strings")
 
-        stringsMap.put(InMemLocation(1), "one", "1")
-        Thread.sleep(100)
+    stringsMap.put(InMemLocation(1), "one", "1")
+    Thread.sleep(100)
 
-        stringsMap.put(InMemLocation(2), "two", "2")
-        Thread.sleep(100)
+    stringsMap.put(InMemLocation(2), "two", "2")
+    Thread.sleep(100)
 
-        assert(Some("1") === stringsMap.get("one"))
-        assert(Some("2") === stringsMap.get("two"))
-        assert(Some("1") === RefMap.get(classOf[String], "strings").get("one"))
+    assert(Some("1") === stringsMap.get("one"))
+    assert(Some("2") === stringsMap.get("two"))
+    assert(Some("1") === RefMap.get(classOf[String], "strings").get("one"))
   }
 }
