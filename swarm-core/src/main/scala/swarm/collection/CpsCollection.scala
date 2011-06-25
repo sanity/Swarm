0753f5b033bf23e483db02481873a9d6862919f4
James Earl Douglas <james@earldouglas.com>
2011-06-25 06:37:28 -0700

    add more cps collection goodness

--- swarm-core/src/main/scala/swarm/collection/CpsCollection.scala
+++ swarm-core/src/main/scala/swarm/collection/CpsCollection.scala
@@ -1,5 +1,9 @@
-object CpsCollection {
+import scala.collection.IterableLike
+import scala.collection.GenTraversableOnce
+import scala.collection.generic.CanBuildFrom
+import scala.util.continuations._
 
+object CpsCollection {
   // implicit coversion from iterable-like collections to cps-friendly collections
   implicit def cpsIterable[A, Repr](xs: IterableLike[A, Repr]) = new {
     def cps = new {
@@ -17,7 +21,27 @@ object CpsCollection {
         for (x <- this) b ++= f(x)
         b.result
       }
+      def filter(f: A => Boolean@cpsParam[Unit, Unit])(implicit cbf: CanBuildFrom[Repr, A, Repr]): Repr@cpsParam[Unit, Unit] = {
+        val b = cbf(xs.repr)
+        for (x <- this)
+          if (f(x)) b += x
+        b.result
+      }
+      def foldLeft[B](z: B)(f: (B, A) => B@cpsParam[Unit, Unit]): B@cpsParam[Unit, Unit] = {
+        val it = xs.iterator
+        var acc: B = z
+        while(it.hasNext) acc = f(acc, it.next)
+        acc
+      }
+      def reduceLeft[B >: A](f: (B, A) => B@cpsParam[Unit, Unit]): B@cpsParam[Unit, Unit] = {
+        if (xs.isEmpty)
+          throw new UnsupportedOperationException("empty.reduceLeft")
+
+        val it = xs.iterator
+        var acc: B = it.next
+        while(it.hasNext) acc = f(acc, it.next)
+        acc
+      }
     }
   }
 }
-
