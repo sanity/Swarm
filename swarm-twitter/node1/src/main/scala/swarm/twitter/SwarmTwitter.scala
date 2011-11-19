e903f6e42322c456a396088b4385dcaeacf5c4a3
James Earl Douglas <james@earldouglas.com>
2011-11-18 20:03:09 -0800

    remove old swarm-twitter project

--- swarm-twitter/node1/src/main/scala/swarm/twitter/SwarmTwitter.scala
+++
@@ -1,3 +0,0 @@
-package swarm.twitter
-
-class SwarmTwitter extends SwarmTwitterTemplate("node1", 9998, 9997)
--- swarm-twitter/node2/src/main/scala/swarm/twitter/SwarmTwitter.scala
+++
@@ -1,3 +0,0 @@
-package swarm.twitter
-
-class SwarmTwitter extends SwarmTwitterTemplate("node2", 9997, 9998)
